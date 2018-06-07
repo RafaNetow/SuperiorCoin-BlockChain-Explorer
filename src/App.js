1e737f1af7b915ef39ec41f30b3ede8241ef1bfd
Rafael Sequeiros <rafanetow@gmail.com>
2018-06-07 13:24:17 -0600

    Table working correctly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import BootstrapTable from 'reactjs-bootstrap-table';
+import _ from "lodash";
+const products = [];
+const baseUrl = 'http://superior-coin.com:8081/api';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Superior Coin</h1>
-        </header>
-        <p className="App-intro">
-
-        </p>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      currency: {},
+      tableData: [{ 
+        hieght: '14567',
+        txs:'1',
+        time:'11:2700 am',
+        hash: 'avn12c9s8ds9df8v8v90b0asd09b09asd890asd890',
+        age: '200'  
+      }],
+      tableColumns: [ {name: 'hieght'},
+      {name: 'txs'},
+      {name: 'time'},
+      {name: 'hash'},
+      {name: 'age'}
+    ]
+    };
+   this.transaction = this.transaction.bind(this)
   }
+  
+
+  componentDidMount() {
+    this.transaction(0,100);
+    /*
+    let transactionPromese = this.transaction(0,100);
+    let blocksArray = [];
+    transactionPromese.then(function(result) {
+      _.forEach(result.data.blocks, function(block){
+        blocksArray.push({
+          age: block.age,
+          hash:block.hash,
+          height:block.height,
+          time: block.timestamp_utc,
+          txs: block.txs.length        
+        });
+        });  */
+ };
+ /*
   
   blockForNumber(numberOfBlock){
     fetch(`http://superior-coin.com:8081e/api/block/${numberOfBlock}`, {
@@ -23,21 +55,12 @@ class App extends Component {
     })
       .then(res => res.json())
       .then( (responseJson) => {
-        console.log(responseJson);
       })
   }
 
   
 
-  transactions (numerOfPage,limit){
-    fetch(`http://superior-coin.com:8081/api/transactions?page=${numerOfPage}&limit=${limit}`, {
-      Accept: 'application/json'
-    })
-      .then(res => res.json())
-      .then( (responseJson) => {
-        console.log(responseJson);
-      })
-  }
+ 
 
   InformationOfTransaction(hash){
     fetch(`http://superior-coin.com:8081/api/transaction/${hash}`, {
@@ -65,7 +88,6 @@ class App extends Component {
     })
     .then(res => res.json())
     .then( (responseJson) => {
-      console.log(responseJson);
     });
   }
 
@@ -75,7 +97,6 @@ class App extends Component {
     })
     .then(res => res.json())
     .then( (responseJson) => {
-      console.log(responseJson);
     });
   }
 
@@ -99,7 +120,7 @@ class App extends Component {
     });
   }
 
-  SearchForBlockNumberBlockHashOrTxtHash(item){
+  networkInfo(item){
     fetch(`http://superior-coin.com:8081/api/networkinfo`, {
       Accept: 'application/json'
     })
@@ -109,15 +130,50 @@ class App extends Component {
     });
   }
 
-
+  */
+   
  
+  transaction (pageNumber, limit) {
+  const transactionUrl = `${baseUrl}/transactions?page=${pageNumber}&limit=${limit}`
+  const that = this;
+    fetch(transactionUrl)
+      .then(res => res.json())
+      .then((transactionResponse) => {
+        const tableData = [];
+        _.forEach(transactionResponse.data.blocks, function(block){
+          tableData.push({
+            age: block.age,
+            hash:block.hash,
+            hieght:block.height,
+            time: block.timestamp_utc,
+            txs: block.txs.length        
+          });
 
-  ///api/mempool
-
-  componentDidMount() {
-  
-    console.log("Testing this");
+        that.setState( {tableData })
+      });
+    });
   }
+    
+  render() {
+    const { tableData } = this.state;
+    console.log(tableData);
+    return (
+   
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Superior Coin</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+        <BootstrapTable columns={this.state.tableColumns} data={tableData} headers={true}/>
+      </div>
+    );
+  }
+  
+
+
+  
 }
 
 
